f4e6527ddfeeeb47a628943b2ef84e8364a85484
Aryach76 <76828870+Aryach76@users.noreply.github.com>
2025-06-23 09:18:14 +0530

    added cardinality

--- src/main/java/dev/arya/productservice/models/Category.java
+++ src/main/java/dev/arya/productservice/models/Category.java
@@ -2,9 +2,16 @@ package dev.arya.productservice.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Category extends BaseModel{
 
     private String name;
+
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
