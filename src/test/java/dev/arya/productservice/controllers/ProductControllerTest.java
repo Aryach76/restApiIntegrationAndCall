ee1b8bbdb5cbefe6b0353ab6dc0c69f584cb34d6
Aryach76 <76828870+Aryach76@users.noreply.github.com>
2025-07-10 09:54:15 +0530

    adding Mockito for mocking dependencies code

--- src/test/java/dev/arya/productservice/controllers/ProductControllerTest.java
+++ src/test/java/dev/arya/productservice/controllers/ProductControllerTest.java
@@ -1,13 +1,37 @@
 package dev.arya.productservice.controllers;
 
 
+import dev.arya.productservice.dtos.GenericProductDto;
+import dev.arya.productservice.exceptions.NotFoundException;
+import dev.arya.productservice.services.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 
 
+@SpringBootTest
 public class ProductControllerTest {
+
+    @Autowired
+    private ProductController productController;
+
+    @MockitoBean
+    private ProductService productService;
+
+    @Test
+    void returnNullWhenProductDoesNotExist() throws NotFoundException {
+        GenericProductDto genericProductDto=productController.getProductById(121L);
+        when(productService.getProductById(121L))
+                .thenReturn(null);
+
+        assertNull(genericProductDto);
+    }
 
     @Test
     @DisplayName("My Custom Test case test")
